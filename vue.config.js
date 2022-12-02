edf8b6599547ee08b7b983d6cf0dfb6ee1538d3d
RahymCharyev <r569837@gmail.com>
2022-12-02 15:35:39 +0500

    ✨feature: Added pwa to project

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/asteroids-app/" : "/",
+  pwa: {
+    name: "Asteroids App",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // настройки манифеста
+    manifestOptions: {
+      display: "landscape",
+      background_color: "#42B883",
+      // ...другие настройки манифеста...
+    },
+
+    // настройка workbox-плагина
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc необходимо в режиме InjectManifest
+      swSrc: "dev/sw.js",
+      // ...другие настройки Workbox...
+    },
+  },
 };
